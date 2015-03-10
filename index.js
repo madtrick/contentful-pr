6e024cfeb65727477ac41ed9e86bd9ad0bca4fc2
Farruco Sanjurjo <madtrick@gmail.com>
2015-03-10 21:02:26 +0100

    Extract Repository related code to it's own module

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 'use strict';
 
-var gitty       = require('gitty');
 var Promise     = require('bluebird');
 var GithubAPI   = require('github');
 var editor      = require('editor');
@@ -10,51 +9,10 @@ var fs          = require('fs');
 var path        = require('path');
 var tpAPI       = require('tp-api');
 var logger      = require('./lib/logging').logger();
+var Repository  = require('./lib/repository');
 var revalidator = require('revalidator');
 
 var github, targetprocess;
-
-var Repository = Repository;
-function Repository () {
-  this.config = {};
-  this.config.remote = 'origin';
-}
-
-Repository.prototype.setup = function () {
-  var promise = Promise.all([
-    getCurrentBranchName(),
-    getRepoOwner(),
-    getRepoName()
-  ])
-  .bind(this)
-  .then(function (args) {
-    this.config.currentBranchName = args[0];
-    this.config.ownerName         = args[1];
-    this.config.name              = args[2];
-
-    return this;
-  });
-
-  return promise;
-};
-
-Repository.prototype.push = function () {
-  var repo                = repository();
-  var currentBranchName   = this.config.currentBranchName;
-  var remote              = this.config.remote;
-
-  if ( currentBranchName === 'master') {
-    logger.error('Not pushing master');
-    return;
-  }
-
-  return Promise.promisify(repo.push)(remote, currentBranchName, {})
-  .bind(this)
-  .then(function () {
-    logger.info('Pushed branch', currentBranchName);
-    return this;
-  });
-};
 
 var PullRequest = PullRequest;
 function PullRequest (options) {
@@ -229,50 +187,4 @@ function prMessage (template, tpTicket) {
       });
     });
   };
-}
-
-function repository () {
-  var repoPath = process.cwd();
-  var repo = gitty(repoPath);
-
-  return repo;
-}
-
-function getRemote () {
-  var repo = repository();
-
-  return Promise.promisify(repo.getRemotes, repo)();
-}
-
-function getBranches () {
-  var repo = repository();
-
-  return Promise.promisify(repo.getBranches, repo)();
-}
-
-function getCurrentBranchName () {
-  return getBranches()
-  .then(function (branches) {
-    return branches.current;
-  });
-}
-
-function getRepoName () {
-  return getRemote()
-  .then(getRepoNameFromRemote);
-}
-
-function getRepoOwner () {
-  return getRemote()
-  .then(getRepoOwnerFromRemote);
-}
-
-function getRepoNameFromRemote (remote) {
-  var remoteName = remote.origin.match(/\/([\w-]+)\.git$/)[1];
-  return remoteName;
-}
-
-function getRepoOwnerFromRemote (remote) {
-  var repoOwner = remote.origin.match(/\:([\w-]+)\/.+$/)[1];
-  return repoOwner;
 }
