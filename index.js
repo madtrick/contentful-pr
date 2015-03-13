cdd12dbc13077ad1aac4e393aea2aa72d46dc3c0
Farruco Sanjurjo <madtrick@gmail.com>
2015-03-13 17:21:28 +0100

    Extract prMessage module

--- index.js
+++ index.js
@@ -3,14 +3,11 @@
 var Promise     = require('bluebird');
 var gitty       = require('gitty');
 var GithubAPI   = require('github');
-var editor      = require('editor');
-var temp        = require('temp');
-var fs          = require('fs');
-var path        = require('path');
 var tpAPI       = require('tp-api');
 var logger      = require('./lib/logging').logger();
 var Repository  = require('./lib/repository');
 var PullRequest = require('./lib/pull-request');
+var prMessage   = require('./lib/pull-request-message.js');
 var revalidator = require('revalidator');
 
 var github, targetprocess;
@@ -99,37 +96,4 @@ function run (config, options) {
       }
     });
   });
-}
-
-function prMessage (template, tpTicket) {
-  temp.track();
-
-  return function () {
-    return new Promise( function (resolve) {
-      fs.readFile(path.resolve(template), function (err, templateData) {
-        temp.open(null, function (err, info) {
-          var templateString = templateData.toString();
-          var message        = templateString.replace(/:tp-ticket-id:/g, tpTicket);
-
-          var data = [
-            '\n',
-            message
-          ];
-
-          fs.writeFile(info.path, data.join(''), function () {
-            editor(info.path, function () {
-              fs.readFile(info.path, function (error, data) {
-                var message = data.toString();
-                var lines   = message.split('\n');
-                var title   = lines[0];
-                var body    = lines.slice(1, lines.length - 1).join('\n');
-
-                resolve({title: title, body: body});
-              });
-            });
-          });
-        });
-      });
-    });
-  };
 }
