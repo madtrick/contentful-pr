4ad92e5c9ab0fb2a71101ffc98495a56997b46d7
Farruco Sanjurjo <madtrick@gmail.com>
2015-03-13 15:27:31 +0100

    Run tests only for files ending in _tests.js

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,6 @@ var gulp  = require('gulp');
 var mocha = require('gulp-mocha');
 
 gulp.task('test', function () {
-  return gulp.src('tests/**.js', {read: false})
+  return gulp.src('tests/**_tests.js', {read: false})
           .pipe(mocha({reporter: 'nyan'}));
 });
